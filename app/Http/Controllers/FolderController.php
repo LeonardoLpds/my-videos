a43b07bfeacb69622a5fbe4cda8728381a7d1ff2
Leonardo Souza <leonardo.lpds@gmail.com>
2020-08-16 15:27:37 -0300

    Alterando configuração de pastas

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -8,14 +8,14 @@ class FolderController extends Controller
 {
     public function showFolders()
     {
-        $folders = $this->foldersWithVideos(dirname(base_path()));
+        $folders = $this->foldersWithVideos(storage_path('app/videos'));
         return view('folders', ['folders' => $folders]);
     }
 
     public function openFolder(Request $request)
     {
         $folder = $request->query('folder');
-        $path = dirname(base_path()) . "$folder";
+        $path = storage_path('app/videos') . "$folder";
         $videos = scandir($path);
         return view('videos', ['videos' => $videos, 'path' => $path, 'folder' => $folder]);
     }
@@ -34,7 +34,7 @@ class FolderController extends Controller
             if (is_dir($path)) {
                 foreach (scandir($path) as $fileName) {
                     if (is_file($path . '/' . $fileName) && strpos(mime_content_type($path . '/' . $fileName), 'video') !== false) {
-                        $results[] = str_replace(dirname(base_path()), '', $path);
+                        $results[] = str_replace(realpath(storage_path('app/videos')), '', $path);
                         break;
                     }
                 }
--- resources/views/videos.blade.php
+++ resources/views/videos.blade.php
@@ -7,7 +7,7 @@
     <!-- <div class="w-2/5 p-2 h-32">
         <div class="text-white font-bold bg-gray-800 w-full h-full text-center justify-center flex flex-col text-sm"> -->
             <video controls preload="none">
-                <source src="/videos/{{$folder}}/{{$video}}" type="{{mime_content_type($path .'/'. $video)}}">
+                <source src="/videos{{$folder}}/{{$video}}" type="{{mime_content_type($path .'/'. $video)}}">
             </video>
         <!-- </div>
     </div> -->
